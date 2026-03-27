0430a19a60f5dcb41cc01faae99d9f86c9832bd9
Eduardo Alba <eduardoalba0@hotmail.com>
2026-03-27 18:39:12 -0300

    Inclusão dos comentários e referências sobre as Queries.

--- src/dao/ClienteDAO.php
+++ src/dao/ClienteDAO.php
@@ -11,17 +11,22 @@ class ClienteDAO extends GenericDAO
 {
     protected static $modelClass = Cliente::class;
 
+    // Utiliza "Magic Finders" construídas pelo repository do Doctrine  conforme os atributos da classe alvo mapeados
+    // Mais informações no capítulo "Magic Finders" em: https://www.doctrine-project.org/documentation/manual/
     public static function buscarNome($nome)
     {
         try {
             $em = Conexao::getEntityManager();
             $repository = $em->getRepository(Cliente::class);
+            // Usando a chamada "mágica" o doctrine reconhece o "nome" como um atributo de "Cliente" e compreender que desejamos buscar pelo atributo "nome"
+            // Existem diferentes combinações e padrões que podem ser usadas nestas chamadas mágicas.
             return $repository->findByNome($nome);
         } catch (Exception $ex) {
             throw new Exception("Falha ao buscar cliente pelo nome. " . $ex->getMessage());
         }
     }
 
+    // Idem ao anterior, mas ao invés de fazer uma chamada mágica, envia  uma lista de parâmetros e valores para serem filtrados
     public static function buscarNome2($nome)
     {
         try {
@@ -33,6 +38,8 @@ class ClienteDAO extends GenericDAO
         }
     }
 
+    // Utiliza abordagens de construção da consulta por meio de funções do repositório Doctrine
+    // Mais informações em: https://www.doctrine-project.org/projects/doctrine-orm/en/3.6/reference/query-builder.html
     public static function buscarNomeQueryBuilder($nome){
         try {
             $em = Conexao::getEntityManager();
@@ -48,6 +55,8 @@ class ClienteDAO extends GenericDAO
     }
 
 
+    // Utiliza DQL - Doctrine Query Language
+    // Mais informações em: https://www.doctrine-project.org/projects/doctrine-orm/en/3.6/reference/dql-doctrine-query-language.html
     public static function buscarNomeParecido($nome){
         try {
             $em = Conexao::getEntityManager();
