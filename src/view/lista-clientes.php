8e448bbdcc692af59fc0b3523410c8931be38a81
Eduardo Alba <eduardoalba0@hotmail.com>
2026-04-13 23:18:03 -0300

    Rotas de Listar e Buscar clientes com views associadas.

--- src/view/lista-clientes.php
+++ src/view/lista-clientes.php
@@ -14,7 +14,6 @@
     <th>#</th>
     <th>Nome</th>
     <th>CPF</th>
-    <th>Opções</th>
     </thead>
     <tbody>
     <?php foreach ($clientes as $cliente) : ?>
@@ -22,6 +21,7 @@
         <?= "<td>{$cliente->getId()}</td>" ?>
         <?= "<td>{$cliente->getNome()}</td>" ?>
         <?= "<td>{$cliente->getCpf()}</td>" ?>
+        <!-- TODO OPÇÕES DE VISUALIZAR, REMOVER ETC.-->
         <?= "</tr>" ?>
     <?php endforeach; ?>
     </tbody>
